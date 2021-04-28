17fe15f70528b7f690d3012113ccf2abdfbf6bc7
shabanichtbfob <paweiyun56257@163.com>
2021-04-28 20:50:30 +0800

    Clear database state when executing App

--- src/main/java/org/videostore/App.java
+++ src/main/java/org/videostore/App.java
@@ -1,15 +1,25 @@
 package org.videostore;
 
-import pt.ist.fenixframework.Atomic;
-import pt.ist.fenixframework.Atomic.TxMode;
+import javax.transaction.NotSupportedException;
+import javax.transaction.SystemException;
+
+import pt.ist.fenixframework.FenixFramework;
+import pt.ist.fenixframework.core.WriteOnReadError;
 
 public class App {
-	@Atomic(mode = TxMode.WRITE)
 	public static void main(String[] args) {
-		Movie newRelease = new Movie("Jaws", 1);
-		Rental rental1 = new Rental(newRelease, 3);
-		Customer customer = new Customer("Mary");
-		customer.addRental(rental1);
-		System.out.println(customer.statement());
+		try {
+			FenixFramework.getTransactionManager().begin(false);
+			Movie newRelease = new Movie("Jaws", 1);
+			Rental rental1 = new Rental(newRelease, 3);
+			Customer customer = new Customer("Mary");
+			customer.addRental(rental1);
+			System.out.println(customer.statement());
+			FenixFramework.getTransactionManager().rollback();
+			FenixFramework.shutdown();
+		} catch (WriteOnReadError | NotSupportedException | SystemException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
